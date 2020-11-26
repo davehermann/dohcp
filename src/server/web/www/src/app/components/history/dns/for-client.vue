164d984c98cfd986bc744aa8d9a7d05a7bc2614b
Dave Hermann <6787333+davehermann@users.noreply.github.com>
2020-11-26 04:27:27 -0500

    Add sorted DNS request history

--- src/server/web/www/src/app/components/history/dns/for-client.vue
+++ src/server/web/www/src/app/components/history/dns/for-client.vue
@@ -1,12 +1,36 @@
 <template>
     <div class="for_client_component">
-        <div v-if="!!historyForIp" class="panel is-info">
+        <div v-if="!!sortedHistory" class="panel is-info">
             <div class="panel-heading">
                 Recent DNS Request History for {{currentIpHistory}}
             </div>
-            <div v-for="(request, idx) in historyForIp" :key="request.question" class="panel-block column request_list" @click.stop.prevent="ToggleRequests(request)">
+            <div class="panel-block">
+                <div class="field is-grouped is-grouped-multiline">
+                    <div class="control">
+                        <div class="tags has-addons">
+                            <span class="tag" :class="{ 'is-info': sortOrder == 'last-used' }" @click.stop.prevent="ChangeSort('last-used')">Last Used</span>
+                            <span class="tag" :class="{ 'is-info': sortOrder == `reverse-last-used` }" @click.stop.prevent="ChangeSort('reverse-last-used')">Reverse Last Used</span>
+                        </div>
+                    </div>
+
+                    <div class="control">
+                        <div class="tags has-addons">
+                            <span class="tag" :class="{ 'is-info': sortOrder == 'by-domain' }" @click.stop.prevent="ChangeSort('by-domain')">Domain Name</span>
+                            <span class="tag" :class="{ 'is-info': sortOrder == `reverse-by-domain` }" @click.stop.prevent="ChangeSort('reverse-by-domain')">Reverse Domain Name</span>
+                        </div>
+                    </div>
+
+                    <div class="control">
+                        <div class="tags has-addons">
+                            <span class="tag" :class="{ 'is-info': sortOrder == 'by-request-count' }" @click.stop.prevent="ChangeSort('by-request-count')">Number of Requests</span>
+                            <span class="tag" :class="{ 'is-info': sortOrder == `reverse-by-request-count` }" @click.stop.prevent="ChangeSort('reverse-by-request-count')">Reverse Number of Requests</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div v-for="(request, idx) in sortedHistory" :key="request.question" class="panel-block column request_list" @click.stop.prevent="ToggleRequests(request)">
                 <div>
-                    {{historyForIp.length - idx}})
+                    {{sortedHistory.length - idx}})
                     {{request.question}}
                     -
                     {{request.requests.length}} time{{request.requests.length == 1 ? "" : "s"}}
@@ -23,7 +47,7 @@
 </template>
 
 <script>
-    import { ref, watchEffect } from "vue";
+    import { ref, watchEffect, computed } from "vue";
     import { useRouter } from "vue-router";
 
     export default {
@@ -33,6 +57,8 @@
 
             const currentIpHistory = ref(null),
                 historyForIp = ref(null);
+
+            const sortOrder = ref("last-used");
 
             const LoadHistoryForIp = async (ipAddress) => {
                 currentIpHistory.value = ipAddress;
@@ -58,13 +84,52 @@
                 }
             });
 
+            const sortedHistory = computed(() => {
+                if (!historyForIp.value)
+                    return null;
+
+                const ipHistory = historyForIp.value.filter(() => true),
+                    sortBy = sortOrder.value;
+
+                switch (sortBy) {
+                    case "reverse-last-used":
+                        ipHistory.reverse();
+                        break;
+
+                    case "by-domain":
+                        ipHistory.sort((a, b) => (a.question.toLowerCase() < b.question.toLowerCase() ? -1 : 1));
+                        break;
+
+                    case "reverse-by-domain":
+                        ipHistory.sort((a, b) => (a.question.toLowerCase() > b.question.toLowerCase() ? -1 : 1));
+                        break;
+
+                    case "by-request-count":
+                        ipHistory.sort((a, b) => (a.requests.length < b.requests.length ? -1 : 1));
+                        break;
+
+                    case "reverse-by-request-count":
+                        ipHistory.sort((a, b) => (a.requests.length > b.requests.length ? -1 : 1));
+                        break;
+                }
+
+                // By default last-used
+                return ipHistory;
+            });
+
+            const ChangeSort = newSort => {
+                sortOrder.value = newSort;
+            };
+
             const ToggleRequests = request => {
                 request.showRequests = !request.showRequests;
             };
 
             return {
                 currentIpHistory,
-                historyForIp,
+                sortedHistory,
+                sortOrder,
+                ChangeSort,
                 ToggleRequests,
             };
         },
@@ -76,4 +141,5 @@
     /* .for_client_component >>> .class+id {} */
     .request_list { align-items: flex-start; }
     .request_list .request { margin-left: 2em; font-size: 0.75em;}
+    .tag { cursor: pointer; }
 </style>
